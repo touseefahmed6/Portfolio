3e9d3b80683c7ce2b3da057fa99855ba92085b0a
touseefahmed6 <touseefahmedhalepoto@gmail.com>
2025-02-14 18:35:13 +0500

    Fixed

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/no-unknown-property */
 import { useState, useRef, Suspense } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
 
-const Stars = (props) => {
+const Stars = () => {
   const ref = useRef(null);
   const [sphere] = useState(() => random.inSphere(new Float32Array(8000), { radius: 1.75 }));
 
